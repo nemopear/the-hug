47e0428dda1f558b248ee2e9b04e4ea7f6ffeb3e
nutchaya@opendream.co.th <nutchaya@opendream.co.th>
2018-01-26 00:29:38 +0700

    fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,31 @@
 $(function(){
-    var url = window.location.href;
-    var activeTab = url.substring(url.indexOf("#") + 1);
+    // var url = window.location.href;
+    // var activeTab = url.substring(url.indexOf("#") + 1);
+    // $(".tab-pane").removeClass("active in show");
 
-    console.log('activeTab' + activeTab);
+    
+    // $("#" + activeTab).addClass("active in show");
+    // console.log($("#" + activeTab));
+    // $('.serviceTab .nav-link').removeClass('active');
+    // $('a[href="#'+ activeTab +'"]').addClass('active');
 
-    $(".tab-pane").removeClass("active in show");
-    $("#" + activeTab).addClass("active in show");
-    $('.serviceTab .nav-link').removeClass('active');
-    $('a[href="#'+ activeTab +'"]').addClass('active');
-
-    $(".nav-tabs").find("li a").last().click();
+    // $(".nav-tabs").find("li a").last().click();
     
-    var url = document.URL;
-    var hash = url.substring(url.indexOf('#'));
+    // var url = document.URL;
+    // var hash = url.substring(url.indexOf('#'));
     
-    $(".nav-tabs").find("li a").each(function(key, val) {
+    // $(".nav-tabs").find("li a").each(function(key, val) {
     
-      if (hash == $(val).attr('href')) {
+    //   if (hash == $(val).attr('href')) {
     
-        $(val).click();
+    //     $(val).click();
     
-      }
-      $(val).click(function(ky, vl) {
+    //   }
+    //   $(val).click(function(ky, vl) {
     
-        console.log($(this).attr('href'));
-        location.hash = $(this).attr('href');
-      });
+    //     console.log($(this).attr('href'));
+    //     location.hash = $(this).attr('href');
+    //   });
     
-    });
+    // });
   });
